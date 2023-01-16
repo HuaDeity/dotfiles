38c5902293426f71319f588e1233f32b4b9394f8
HuaDeity <tidy.year0352@fastmail.com>
2023-01-17 01:05:30 +0800

    ohmyz

--- dot_config/nvim/lua/plugin_config/executable_bufferline.lua
+++
@@ -1,9 +0,0 @@
-require('bufferline').setup{
-  options = {
-    close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    offsets = { { filetype = "NvimTree", text = "File Tree", padding = 1} },
-    separator_style = "thin", -- | "thick" | "slant" | { 'any', 'any' },
-  },
-}
-
