81006d2c72028af7f93bfc0fa286cddb7a2c70c9
HuaDeity <hello@huadeity.com>
2025-04-27 00:08:47 +0800

    Add password manager init

---
+++ .install-password-manager.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# exit immediately if doppler is already in $PATH
+type doppler >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    brew install doppler
+    ;;
+Linux)
+    nix shell "nixpkgs#doppler"
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
