5a94ffb34bcc33cc23e67e9749e5ffb4f47740c4
HuaDeity <hello@huadeity.com>
2025-05-03 00:09:59 +0800

    Make python experience better

--- home/dot_config/nvim/lua/plugins/lang.lua
+++ home/dot_config/nvim/lua/plugins/lang.lua
@@ -4,35 +4,8 @@ return {
     enabled = function()
       return LazyVim.has_extra("lang.python")
     end,
-    opts = {
-      search = {
-        cwd = false,
-      },
-    },
   },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        python = { "ruff" },
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        python = {
-          -- To fix auto-fixable lint errors.
-          "ruff_fix",
-          -- To run the Ruff formatter.
-          "ruff_format",
-          -- To organize the imports.
-          "ruff_organize_imports",
-        },
-      },
-    },
-  },
+
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
