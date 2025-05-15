9816046da164c9a7de1559ea40d286838ba92298
HuaDeity <hello@huadeity.com>
2025-05-15 15:36:26 +0800

    Update chezmoi.vim

--- home/private_dot_config/nvim/lua/plugins/util/dotfiles.lua
+++ home/private_dot_config/nvim/lua/plugins/util/dotfiles.lua
@@ -36,10 +36,8 @@ return {
   {
     -- highlighting for chezmoi files template files
     "alker0/chezmoi.vim",
-    init = function()
-      vim.g["chezmoi#use_tmp_buffer"] = 1
-      vim.g["chezmoi#source_dir_path"] = os.getenv "HOME" .. "/.local/share/chezmoi"
-    end,
+    lazy = false,
+    init = function() vim.g["chezmoi#use_tmp_buffer"] = true end,
   },
   {
     "xvzc/chezmoi.nvim",
