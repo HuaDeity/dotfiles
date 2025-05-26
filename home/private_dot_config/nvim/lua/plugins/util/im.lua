3ba54f6236e7f7f983a4227d81528d4c555f385c
HuaDeity <hello@huadeity.com>
2025-05-26 14:13:44 +0000

    disable im in remote
    Signed-off-by: HuaDeity <hello@huadeity.com>

--- home/private_dot_config/nvim/lua/plugins/util/im.lua
+++ home/private_dot_config/nvim/lua/plugins/util/im.lua
@@ -1,7 +1,7 @@
 return {
   {
     "keaising/im-select.nvim",
-    enabled = function() return vim.env.SSH_TTY ~= 1 end,
+    enabled = function() return vim.env.SSH_TTY == nil end,
     lazy = false,
     opts = {},
   },
