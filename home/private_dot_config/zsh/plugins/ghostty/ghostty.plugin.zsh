342e3f422320d91a53b98c4e05b3a1c035836186
HuaDeity <hello@huadeity.com>
2025-05-29 04:18:50 +0000

    Add Ghostty Features for SSH

--- home/private_dot_config/zsh/plugins/ghostty/ghostty.plugin.zsh
+++ home/private_dot_config/zsh/plugins/ghostty/ghostty.plugin.zsh
@@ -1,4 +1,5 @@
 export GHOSTTY_RESOURCES_DIR="${GHOSTTY_RESOURCES_DIR:-$XDG_CONFIG_HOME/ghostty}"
+export GHOSTTY_SHELL_FEATURES="${GHOSTTY_SHELL_FEATURES:-cursor,sudo,title}"
 
 if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
   source "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
