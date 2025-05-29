5dd295174794829cec8e08a326dfb07794b0b0c8
HuaDeity <hello@huadeity.com>
2025-05-29 12:01:47 +0800

    Add custom ghostty shell integration for ssh use

--- home/private_dot_config/zsh/plugins/ghostty/ghostty.plugin.zsh
+++ home/private_dot_config/zsh/plugins/ghostty/ghostty.plugin.zsh
@@ -1,3 +1,5 @@
+export GHOSTTY_RESOURCES_DIR="${GHOSTTY_RESOURCES_DIR:-$XDG_CONFIG_HOME/ghostty}"
+
 if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
   source "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
 fi
