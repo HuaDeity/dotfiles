46174cfb9e226f3ec85d5d913ba14cbd4c68ccec
HuaDeity <hello@huadeity.com>
2025-06-09 10:47:58 +0800

    Update

--- home/private_dot_config/fish/conf.d/direnv.fish
+++ home/private_dot_config/fish/conf.d/direnv.fish
@@ -1,3 +1,4 @@
 if command -q direnv
+    set -gx DIRENV_LOG_FORMAT (set_color --dim)"direnv: %s"(set_color normal)
     direnv hook fish | source
 end
--- home/private_dot_config/flake/hosts/darwin/default.nix
+++ home/private_dot_config/flake/hosts/darwin/default.nix
@@ -71,12 +71,6 @@ with lib;
 
   system = {
     defaults = {
-      CustomUserPreferences = {
-        NSGlobalDomain = {
-          AppleKeyboardUIMode = 2; # TODO: after nix fix can use official namespace
-        };
-      };
-
       dock = {
         autohide = true;
         show-recents = true;
@@ -110,8 +104,9 @@ with lib;
       };
 
       NSGlobalDomain = {
-        AppleShowAllExtensions = false;
+        AppleKeyboardUIMode = 2;
         ApplePressAndHoldEnabled = false;
+        AppleShowAllExtensions = false;
 
         KeyRepeat = 6; # Values: 120, 90, 60, 30, 12, 6, 2
         InitialKeyRepeat = 35; # Values: 120, 94, 68, 35, 25, 15
