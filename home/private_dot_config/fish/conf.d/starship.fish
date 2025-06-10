1026075025a12069f7acb3a14c0daa0949bc214a
HuaDeity <hello@huadeity.com>
2025-06-11 03:11:02 +0800

    Use starship

--- home/private_dot_config/fish/conf.d/starship.fish
+++ home/private_dot_config/fish/conf.d/starship.fish
@@ -6,6 +6,6 @@ if command -q starship
     # function starship_transient_rprompt_func
     #     starship module time
     # end
-    # starship init fish | source
-    # enable_transience
+    starship init fish | source
+    enable_transience
 end
