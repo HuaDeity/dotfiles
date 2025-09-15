b50764b17b90c1b8f2ff2438e3e3d4e5ffa1dbe6
HuaDeity <hello@huadeity.com>
2025-09-16 01:46:51 +0800

    Flake: split to another repo

--- home/.chezmoiscripts/packages/run_onchange_rust.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v "rustup" &>/dev/null; then
-  rustup default stable
-  if ! rust-analyzer --version &>/dev/null; then
-    rustup component add rust-analyzer
-  fi
-  rustup update
-fi
