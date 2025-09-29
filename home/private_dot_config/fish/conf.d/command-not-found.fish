44304c7ae790a46ea509c7cd4d599b69b5e85456
HuaDeity <hello@huadeity.com>
2025-09-29 21:55:47 +0800

    Shell: Fix homebrew command-not-found api change

--- home/private_dot_config/fish/conf.d/command-not-found.fish
+++ home/private_dot_config/fish/conf.d/command-not-found.fish
@@ -1,6 +1,6 @@
 switch (uname -s)
     case Darwin
-        set -l __hb_cnf_handler (brew --repository)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
+        set -l __hb_cnf_handler (brew --repository)"/Library/Homebrew/command-not-found/handler.fish"
         if test -f $__hb_cnf_handler
             source $__hb_cnf_handler
         end
--- home/private_dot_config/zsh/conf.d/command-not-found.zsh
+++ home/private_dot_config/zsh/conf.d/command-not-found.zsh
@@ -1,7 +1,7 @@
 ## Platforms with a built-in command-not-found handler init file
 
 if [[ OS == "Darwin" ]]; then
-	HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
+	HB_CNF_HANDLER="$(brew --repository)/Homebrew/command-not-found/handler.sh"
 	if [ -f "$HB_CNF_HANDLER" ]; then
 		source "$HB_CNF_HANDLER";
 	fi
